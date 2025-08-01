c44743243493b995957377798976ea7918d26119
m4rtinzz <contatotin41@gmail.com>
2025-08-01 11:22:35 -0300

    EmailService.ts

--- poo-abstracao/src/App.tsx
+++ poo-abstracao/src/App.tsx
@@ -1,13 +1,11 @@
-import { useState } from 'react'
 import './App.css'
+import { EmailService } from './services/EmailService';
 
 function App() {
+  const email = new EmailService();
+  email.enviar('lucas@exemplo.com', 'Boas-vindas', 'Olá, Lucas!');
 
-  return (
-    <>
-      
-    </>
-  )
+  return <p>E-mail enviado com sucesso!</p>;
 }
 
 export default App
