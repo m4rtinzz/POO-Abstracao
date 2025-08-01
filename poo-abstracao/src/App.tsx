1c7661874d7f31ffa750ba95d94c09d48011f820
m4rtinzz <contatotin41@gmail.com>
2025-08-01 11:43:11 -0300

    Usuário escolhe os dados do e-mail

--- poo-abstracao/src/App.tsx
+++ poo-abstracao/src/App.tsx
@@ -1,11 +1,42 @@
 import './App.css'
+import { useState } from 'react';
 import { EmailService } from './services/EmailService';
 
 function App() {
-  const email = new EmailService();
-  email.enviar('lucas@exemplo.com', 'Boas-vindas', 'Olá, Lucas!');
+  const [destinatario, setDestinatario] = useState('');
+  const [assunto, setAssunto] = useState('');
+  const [corpo, setCorpo] = useState('');
+  const [enviado, setEnviado] = useState(false);
 
-  return <p>E-mail enviado com sucesso!</p>;
+  const handleEnviar = () => {
+    const email = new EmailService();
+    email.enviar(destinatario, assunto, corpo);
+    setEnviado(true);
+  };
+
+  return (
+    <div>
+      <input
+        type="email"
+        placeholder="Destinatário"
+        value={destinatario}
+        onChange={e => setDestinatario(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Assunto"
+        value={assunto}
+        onChange={e => setAssunto(e.target.value)}
+      />
+      <textarea
+        placeholder="Corpo do e-mail"
+        value={corpo}
+        onChange={e => setCorpo(e.target.value)}
+      />
+      <button onClick={handleEnviar}>Enviar</button>
+      {enviado && <p>E-mail enviado com sucesso!</p>}
+    </div>
+  );
 }
 
 export default App
